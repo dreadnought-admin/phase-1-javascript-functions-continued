0d1882a15af019b3dcc5d8b2970dfb7391619815
dom alexander <dom.alexan@gmail.com>
2022-10-31 22:32:27 -0600

    commit tree c5954f5fa95e87470778b9a02f6caab8a26250ac

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+};
+
+console.log(saturdayFun());
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+};
+
+console.log(mondayWork());
+
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  };
+
+console.log(wrapAdjective());
